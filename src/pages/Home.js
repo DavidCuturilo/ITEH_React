f768a6100864d9bac614a2d8a73d968c657410bf
DavidCuturilo <david.cuturilo@blinking.id>
2022-05-12 23:29:53 +0200

    small fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../App.css'
+import Cards from '../components/Cards';
 import Section from '../components/Section';
-import Cards from '../Cards'
 
 
 function Home(){
